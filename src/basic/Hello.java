ab9d3d5b3e1f94d10fccb8e432e31edf02931742
Puck <qoqjatjr7922@gmail.com>
2022-03-29 12:16:25 +0900

    주사위

--- src/basic/Hello.java
+++ src/basic/Hello.java
@@ -1,10 +1,29 @@
 package basic;
 
 public class Hello {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num = (int)(Math.random() * 6) +1;
+		
+		switch(num) {
+		case 1:
+			System.out.println("1 out");
+			break;
+		case 2:
+			System.out.println("2 out");
+			break;
+		case 3:
+			System.out.println("3 out");
+			break;
+		case 4:
+			System.out.println("4 out");
+			break;
+		case 5:
+			System.out.println("5 out");
+			break;
+		case 6:
+			System.out.println("6 out");
+			break;
+						
+		}
 	}
-
 }
